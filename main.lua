5a53bae1cbe27a08cce441d9d98272cfd596cdfe
Melzinoff <antoniomelzinoff@hotmail.fr>
2024-12-07 23:30:52 +0100

    Added SnowKing
    Melzinoff's contribution to the collab, voice acting by Pyronoid!, went all in and added power meter, caps, palettes and voice acted.

--- main.lua
+++ main.lua
@@ -11,11 +11,12 @@ end
 local E_MODEL_WHITEY = smlua_model_util_get_id("Whitey_geo")
 local E_MODEL_MUNKSHROOM = smlua_model_util_get_id("alvin_munkshroom_geo")
 local E_MODEL_ARTHUR = smlua_model_util_get_id("arthurxmas_geo")
+local E_MODEL_SNOWKING = smlua_model_util_get_id("snowking_geo")
 
 local TEX_WHITEY_LIFE_ICON = get_texture_info("whitey") -- Located in "textures"
 local TEX_MUNKSHROOM_LIFE_ICON = get_texture_info("alvin") -- Located in "textures"
 local TEX_ARTHUR_LIFE_ICON = get_texture_info("arthurxmas") -- Located in "textures"
-
+local TEX_SNOWKING = get_texture_info("snowking_icon")
 
 local VOICETABLE_WHITEY = {
     [CHAR_SOUND_ATTACKED] = 'WhiteyHurtAAA.ogg',
@@ -79,6 +80,49 @@ local VOICETABLE_MUNKSHROOM = {
     [CHAR_SOUND_YAWNING] = 'MUNKSHROOM_SOUND_IMA_TIRED',
 }
 
+local VOICETABLE_SNOWKING = {
+    [CHAR_SOUND_ATTACKED] = {'sk_CHAR_SOUND_ATTACKED.ogg'},
+    [CHAR_SOUND_COUGHING1] = 'sk_CHAR_SOUND_COUGHING1.ogg',
+    [CHAR_SOUND_COUGHING2] = 'sk_CHAR_SOUND_COUGHING2.ogg',
+    [CHAR_SOUND_COUGHING3] = 'sk_CHAR_SOUND_COUGHING3.ogg',
+    [CHAR_SOUND_DOH] = {'sk_CHAR_SOUND_DOH.ogg'},
+    [CHAR_SOUND_DROWNING] = 'sk_CHAR_SOUND_DROWNING.ogg',
+    [CHAR_SOUND_DYING] = 'sk_CHAR_SOUND_DYING.ogg',
+    [CHAR_SOUND_EEUH] = 'sk_CHAR_SOUND_EEUH.ogg',
+    [CHAR_SOUND_GROUND_POUND_WAH] = 'sk_CHAR_SOUND_GROUND_POUND_WAH.ogg',
+    [CHAR_SOUND_HAHA] = 'sk_CHAR_SOUND_HAHA.ogg',
+    [CHAR_SOUND_HAHA_2] = 'sk_CHAR_SOUND_HAHA_2.ogg',
+    [CHAR_SOUND_HERE_WE_GO] = 'sk_CHAR_SOUND_HERE_WE_GO.ogg',
+    [CHAR_SOUND_HOOHOO] = {'sk_CHAR_SOUND_HOOHOO1.ogg', 'sk_CHAR_SOUND_HOOHOO2.ogg','sk_CHAR_SOUND_HOOHOO3.ogg'},
+    [CHAR_SOUND_HRMM] = 'sk_CHAR_SOUND_HRMM.ogg',
+    [CHAR_SOUND_IMA_TIRED] = 'sk_CHAR_SOUND_IMA_TIRED.ogg',
+    [CHAR_SOUND_MAMA_MIA] = 'sk_CHAR_SOUND_MAMA_MIA.ogg',
+    [CHAR_SOUND_LETS_A_GO] = 'sk_CHAR_SOUND_LETS_A_GO.ogg',
+    [CHAR_SOUND_ON_FIRE] = 'sk_CHAR_SOUND_ON_FIRE.ogg',
+    [CHAR_SOUND_OOOF] = 'sk_CHAR_SOUND_OOOF.ogg',
+    [CHAR_SOUND_OOOF2] = 'sk_CHAR_SOUND_OOOF2.ogg',
+    [CHAR_SOUND_PANTING] = 'sk_CHAR_SOUND_PANTING.ogg',
+    [CHAR_SOUND_PANTING_COLD] = 'sk_CHAR_SOUND_PANTING.ogg',
+    [CHAR_SOUND_PUNCH_HOO] = 'sk_CHAR_SOUND_PUNCH_HOO.ogg',
+    [CHAR_SOUND_PUNCH_WAH] = 'sk_CHAR_SOUND_PUNCH_WAH.ogg',
+    [CHAR_SOUND_PUNCH_YAH] = 'sk_CHAR_SOUND_PUNCH_YAH.ogg',
+    [CHAR_SOUND_SO_LONGA_BOWSER] = 'sk_CHAR_SOUND_SO_LONGA_BOWSER.ogg',
+    [CHAR_SOUND_SNORING1] = 'sk_CHAR_SOUND_SNORING1.ogg',
+    [CHAR_SOUND_SNORING2] = 'sk_CHAR_SOUND_SNORING2.ogg',
+    [CHAR_SOUND_SNORING3] = {'sk_CHAR_SOUND_SNORING2.ogg', 'sk_CHAR_SOUND_SNORING1.ogg', 'sk_CHAR_SOUND_SNORING3.ogg'},
+    [CHAR_SOUND_TWIRL_BOUNCE] = 'sk_CHAR_SOUND_TWIRL_BOUNCE.ogg',
+    [CHAR_SOUND_UH] = 'sk_CHAR_SOUND_UH.ogg',
+    [CHAR_SOUND_UH2] = 'sk_CHAR_SOUND_UH.ogg',
+    [CHAR_SOUND_UH2_2] = 'sk_CHAR_SOUND_UH.ogg',
+    [CHAR_SOUND_WAAAOOOW] = 'sk_CHAR_SOUND_WAAAOOOW.ogg',
+    [CHAR_SOUND_WAH2] = 'sk_CHAR_SOUND_GROUND_POUND_WAH.ogg',
+    [CHAR_SOUND_WHOA] = 'sk_CHAR_SOUND_WHOA.ogg',
+    [CHAR_SOUND_YAHOO] = 'sk_CHAR_SOUND_YAHOO.ogg',
+    [CHAR_SOUND_YAWNING] = 'sk_CHAR_SOUND_YAWNING.ogg',
+    [CHAR_SOUND_YAHOO_WAHA_YIPPEE] = { 'sk_CHAR_SOUND_YAHOO_WAHA_YIPPEE1.ogg', 'sk_CHAR_SOUND_YAHOO_WAHA_YIPPEE2.ogg', 'sk_CHAR_SOUND_YAHOO_WAHA_YIPPEE3.ogg'},
+    [CHAR_SOUND_YAH_WAH_HOO] = { 'sk_CHAR_SOUND_YAH_WAH_HOO1.ogg', 'sk_CHAR_SOUND_YAH_WAH_HOO2.ogg', 'sk_CHAR_SOUND_YAH_WAH_HOO3.ogg'},
+    }
+
 local PALETTE_WHITEY = {
     [PANTS]  = "0066cc",
     [SHIRT]  = "feebf7",
@@ -117,6 +161,41 @@ local PALETTE_WHITEY = {
 }
 ]]--
 
+local PALETTE_SNOWKING = {
+    [SHIRT] = "196788",
+    [SHOES] = "992221",
+    [HAIR] = "ffffff",
+    [CAP] = "5A93F7",
+    [PANTS] = "26FC26",
+    [GLOVES] = "D6E1EB",
+    [SKIN] = "C3942F",
+    [EMBLEM] = "5A93F7",
+}
+
+local capSNOWKING = {
+    normal = smlua_model_util_get_id("snowking_cap_geo"),
+    wing = smlua_model_util_get_id("snowking_wing_cap_geo"),
+    metal = smlua_model_util_get_id("snowking_metal_cap_geo"),
+    metalWing = smlua_model_util_get_id("snowking_metal_wing_cap_geo")
+}
+
+local PM_SNOWKING= {
+    label = {
+        left = get_texture_info("snowking_PM1"),
+        right = get_texture_info("snowking_PM2"),
+    },
+    pie = { 
+        [1] = get_texture_info("PM-1"),
+        [2] = get_texture_info("PM-2"),
+        [3] = get_texture_info("PM-3"),
+        [4] = get_texture_info("PM-4"),
+        [5] = get_texture_info("PM-5"),
+        [6] = get_texture_info("PM-6"),
+        [7] = get_texture_info("PM-7"),
+        [8] = get_texture_info("PM-8"),
+        
+    }
+}
 
 local CSloaded = false
 local function on_character_select_load()
@@ -127,6 +206,10 @@ local function on_character_select_load()
     _G.charSelect.character_add_voice(E_MODEL_MUNKSHROOM, VOICETABLE_MUNKSHROOM)
     CT_ARTHURXMAS = _G.charSelect.character_add("Arthur", {"Christmas will be magical", "Just like in a fairy tale."}, "CT Himself", {r = 255, g = 255, b = 255}, E_MODEL_ARTHUR, CT_MARIO, TEX_ARTHUR_LIFE_ICON)
     _G.charSelect.character_add_palette_preset(E_MODEL_ARTHUR, PALETTE_ARTHUR)
+    CT_SNOWKING = _G.charSelect.character_add("Snow King", "Part of the CS Winter Collab! Here comes the Snow King from Mario Golf: Super Rush! Merry Christmas! VOICED BY Pyronoid!", "Melzinoff, Pyronoid!", {r = 255, g = 200, b = 200}, E_MODEL_SNOWKING, CT_MARIO, TEX_SNOWKING)
+    _G.charSelect.character_add_caps(E_MODEL_SNOWKING, capSNOWKING)
+    _G.charSelect.character_add_palette_preset(E_MODEL_SNOWKING, PALETTE_SNOWKING)
+    _G.charSelect.character_add_health_meter(CT_SNOWKING, PM_SNOWKING)
     CSloaded = true
 end
 
@@ -134,12 +217,14 @@ local function on_character_sound(m, sound)
     if not CSloaded then return end
     if _G.charSelect.character_get_voice(m) == VOICETABLE_WHITEY then return _G.charSelect.voice.sound(m, sound) end
     if _G.charSelect.character_get_voice(m) == VOICETABLE_MUNKSHROOM then return _G.charSelect.voice.sound(m, sound) end
+    if _G.charSelect.character_get_voice(m) == VOICETABLE_SNOWKING then return _G.charSelect.voice.sound(m, sound) end
 end
 
 local function on_character_snore(m)
     if not CSloaded then return end
     if _G.charSelect.character_get_voice(m) == VOICETABLE_WHITEY then return _G.charSelect.voice.snore(m) end
     if _G.charSelect.character_get_voice(m) == VOICETABLE_MUNKSHROOM then return _G.charSelect.voice.snore(m) end
+    if _G.charSelect.character_get_voice(m) == VOICETABLE_SNOWKING then return _G.charSelect.voice.snore(m) end
 end
 
 hook_event(HOOK_ON_MODS_LOADED, on_character_select_load)
